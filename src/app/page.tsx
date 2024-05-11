30385548d455436e2a99252a5af82220872b3f25
zafir3 <pratikmogaveera@gmail.com>
2024-05-11 12:28:38 +0530

    added animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 "use client"
 
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator } from '@/components/ui/command';
-import { Zap } from 'lucide-react';
-import { useEffect, useState } from 'react';
-import { ScrollArea } from "@/components/ui/scroll-area"
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { useDebounce } from '@/lib/hooks';
+import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
 
 export type Results = {
     results: string[],
@@ -17,6 +17,22 @@ export default function Home() {
     const [input, setInput] = useState<string>("")
     const [searchResults, setSearchResults] = useState<Results>()
     const debouncedInput = useDebounce(input)
+
+    const title = {
+        hidden: { y: -100, opacity: 0 },
+        visible: {
+            y: 0, opacity: 1,
+            transition: { type: "spring", bounce: 0.5, duration: 0.5 }
+        }
+    }
+
+    const icon = {
+        hidden: { pathLength: 0, color: "#000", },
+        visible: {
+            pathLength: 1, color: "#fff",
+            transition: { delay: 0.2, duration: 1 }
+        }
+    }
 
     useEffect(() => {
         const fetchData = async () => {
@@ -32,18 +48,38 @@ export default function Home() {
     }, [debouncedInput])
 
     return (
-        <main className="h-svh transition-all bg-background text-foreground w-full p-8 md:p-8 lg:p-12">
+        <main className="h-svh transition-all bg-background text-foreground w-full p-4 md:p-8 lg:p-12">
             <div className="flex flex-col gap-4 items-center pt-40 pb-12 lg:pt-32 lg:pb-16">
-                <h1 className="text-5xl lg:text-7xl font-extrabold inline flex items-center gap-2">
-                    <span className=''>FastAPI</span>
-                    <span>
-                        <Zap strokeWidth={1} className='w-[80px] h-[80px] md:w-[100px] md:h-[100px] lg:w-[120px] lg:h-[120px]' />
+                <div className='flex items-center gap-2'>
+                    <motion.h1 variants={title} initial="hidden" animate="visible" className="text-5xl lg:text-7xl font-extrabold">
+                        FastAPI
+                    </motion.h1>
+                    <span><svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24" height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth={1}
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="lucide lucide-zap w-[80px] h-[80px] md:w-[100px] md:h-[100px] lg:w-[120px] lg:h-[120px]">
+                        <motion.path
+                            variants={icon}
+                            initial="hidden"
+                            animate="visible"
+                            d="M4 14a1 1 0 0 1-.78-1.63l9.9-10.2a.5.5 0 0 1 .86.46l-1.92 6.02A1 1 0 0 0 13 10h7a1 1 0 0 1 .78 1.63l-9.9 10.2a.5.5 0 0 1-.86-.46l1.92-6.02A1 1 0 0 0 11 14z"
+                        >
+                        </motion.path>
+                    </svg>
                     </span>
-                </h1>
+
+                </div>
 
                 <p className='text-center text-[0.8rem] md:text-sm leading-none text-muted-foreground'>
-                    High-performance API built with Hono, Redis, Cloudflare Workers, and Next.js.
-                    <br />Find any country below and receive your response instantly, measured in milliseconds.
+                    <span>High-performance API built with Hono, Redis, and Workers.</span>
+                    <br />
+                    <span className='p-4'>Find any country below and receive your response instantly, measured in milliseconds.</span>
                 </p>
 
                 <div className='max-w-md w-full mt-8'>
